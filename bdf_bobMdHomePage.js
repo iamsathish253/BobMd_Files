4696712173b77282ef1f01f525deef4e55db7419
iamsathish253 <iamsathish253@gmail.com>
2024-02-17 19:17:17 +0530

    [bobMd-2]:Implemented css and js files

---
+++ bdf_bobMdHomePage.js
@@ -0,0 +1,10 @@
+
+function navigateTo()
+{
+
+var naviagteUrl=window.page.Xrm.Utility.getGlobalContext().globalContext.getClientUrl();
+console.log(naviagteUrl);
+alert(naviagteUrl);
+alert("JavaScripr Triggred")
+
+}
