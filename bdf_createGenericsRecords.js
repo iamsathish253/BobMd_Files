7a32c7a8c46d840b69323d0680c4eae236b37ba0
iamsathish253 <iamsathish253@gmail.com>
2024-02-17 16:32:10 +0530

    [bobMd]:creating genrics when project is created

---
+++ bdf_createGenericsRecords.js
@@ -0,0 +1,44 @@
+function projectPostSave(exeCutionContext){
+    debugger;
+
+    var formContext=exeCutionContext.getFormContext();
+    formContext.data.entity.addOnPostSave(createGenericRecords);
+   
+}
+
+function createGenericRecords(exeCutionContext){
+
+    debugger;
+
+    var formContext=exeCutionContext.getFormContext();
+
+    var projectId=formContext.data.entity.getId().replace("{","").replace("}","");
+    console.log(projectId)
+    var projectName=formContext.getAttribute("bdf_projectname").getValue();
+    //var projectEntity=formContext.data.entity.getEntityName();
+
+    if(projectName==null || projectName=="" || projectId=="" || projectId==null){
+        return;
+    }
+
+for (let index = 1; index <=6; index++) {
+
+    var record = {};
+    record["bdf_Projectr@odata.bind"] = "/bdf_projectses("+projectId+")"; // Lookup
+    record.bdf_name = projectName + "-" + "GEN "+ "-" + "PID" + projectId; // Text
+    
+    Xrm.WebApi.createRecord("bdf_generics", record).then(
+        function success(result) {
+            var newId = result.id;
+            console.log(newId);
+        },
+        function(error) {
+            console.log(error.message);
+        }
+    );
+        
+           
+           
+}
+        
+    }
